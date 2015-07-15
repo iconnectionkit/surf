e5242d72718b8e48d4b7093094b6030ed0134fc4
iconnectionkit <iconnectionkit@gmail.com>
2015-07-15 14:11:32 +0200

    Update browser.go

--- browser/browser.go
+++ browser/browser.go
@@ -621,7 +621,7 @@ func (bow *Browser) httpRequest(req *http.Request) error {
 	if err != nil {
 		return err
 	}
-
+	fmt.Println(bow.body)
 	buff := bytes.NewBuffer()
 	dom, err := goquery.NewDocumentFromReader(buff)
 	if err != nil {
